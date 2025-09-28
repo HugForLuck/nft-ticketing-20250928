d4e897a2c1e7f42ea3486deb7297691f5293c807
Bastian Krones <kronesbastian@gmail.com>
2025-09-28 19:06:24 +0200

    adds splashscreen

--- apps/portal/src/app/app.utils.ts
+++ apps/portal/src/app/app.utils.ts
@@ -1,5 +1,5 @@
 import { hasEmailVerifyPath } from 'apps/portal/src/auth/auth.utils';
-import { of } from 'rxjs';
+import { of, tap } from 'rxjs';
 
 export function bootstrapApp() {
   const path = window.location.pathname;
@@ -7,5 +7,18 @@ export function bootstrapApp() {
   // used to bypass auth check
   if (hasEmailVerifyPath(path)) return of();
 
-  return of();
+  return of('').pipe(tap(hideSplashScreen));
+}
+
+export function hideSplashScreen(): void {
+  const splashScreen = document.getElementById('app-splash-screen');
+  if (splashScreen) {
+    // 1. Füge die Fade-Out-Klasse hinzu, um die CSS-Animation zu starten.
+    splashScreen.classList.add('fade-out');
+
+    // 2. Entferne das Element nach der Animation vollständig aus dem DOM.
+    setTimeout(() => {
+      splashScreen.remove();
+    }, 500); // Dauer muss mit der CSS-Transitions-Dauer übereinstimmen
+  }
 }
