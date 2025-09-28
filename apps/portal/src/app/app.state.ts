014c06df73f0f94bacb932c842293d42c4d6e0c7
Bastian Krones <kronesbastian@gmail.com>
2025-09-28 18:39:34 +0200

    adds isReady selector

--- apps/portal/src/app/app.state.ts
+++ apps/portal/src/app/app.state.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Action, State } from '@ngxs/store';
+import { Action, Selector, State } from '@ngxs/store';
 import { SetIsReady } from 'apps/portal/src/app/app.actions';
-import { appStateOptions } from 'apps/portal/src/app/app.config';
+import { appStateDefaults } from 'apps/portal/src/app/app.config';
 import { Context, IAppState } from 'apps/portal/src/app/app.model';
 
-@State<IAppState>(appStateOptions)
+@State<IAppState>({
+  name: 'app',
+  defaults: appStateDefaults,
+})
 @Injectable()
 export class AppState {
+  @Selector()
+  static isReady(state: IAppState) {
+    return state.isReady;
+  }
+
   @Action(SetIsReady)
   setIsReady(ctx: Context, { isReady }: SetIsReady) {
     ctx.patchState({ isReady });
