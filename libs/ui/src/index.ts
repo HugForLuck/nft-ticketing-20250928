598634031556da0d873ecccb14ed9db14185e737
Bastian Krones <kronesbastian@gmail.com>
2025-09-28 17:16:23 +0200

    adds a libt to monorepo

---
+++ libs/ui/src/index.ts
@@ -0,0 +1,4 @@
+// Dies ist der öffentliche Einstiegspunkt für die Library.
+// Alles, was hier exportiert wird, kann von anderen Projekten importiert werden.
+export * from './say-hello';
+
---
+++ libs/ui/src/say-hello.ts
@@ -0,0 +1,7 @@
+/**
+ * Eine einfache Test-Funktion, die einen String zurückgibt.
+ * @returns Eine Grußnachricht aus der Library.
+ */
+export function sayHello(): string {
+  return 'Hallo aus der @libs/ui-controls Library!';
+}
